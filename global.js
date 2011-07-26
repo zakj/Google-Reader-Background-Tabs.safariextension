47714ba1a4c495c04994a481b362f6858e22b284
Zak Johnson <zakj@nox.cx>
2011-07-25 18:00:47 -0700

    Remove non-web content

--- global.js
+++
@@ -1,9 +0,0 @@
-function messageHandler(event) {
-    if (event.name === 'openBackgroundTab') {
-        var tab = safari.application.activeBrowserWindow.openTab('background');
-        tab.url = event.message;
-    }
-}
-
-
-safari.application.addEventListener('message', messageHandler, false);
